e3968acd22a61f0fb6bad6ac3ce0a1503cf409ff
mikkype <miguelcaceda@gmail.com>
2023-11-06 18:34:58 +0100

    actualizaciones de toda la web y

---
+++ js/carta.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", init)
+
+function init() {
+    console.log('DOM cargado')
+    //creamos una variable con el id
+    var mostrar = document.getElementById('container-carta');
+    var opciones = {
+        method: 'POST'
+    }
+    //ruta donde se consume el json
+    fetch('json/carta.json')
+        .then(result => result.json())
+        .then(data => {
+            console.log(data)
+            if (Array.isArray(data.carta)) {
+
+
+
+                data.carta.forEach(elemento => {
+
+                    mostrar.innerHTML += 
+                            `
+                            <p>${elemento.Nombre}</p>
+                            <p>${elemento.Descripción}</p>
+                            <p>${elemento.Precio}</p>
+                            `;
+
+                });
+            }
+
+
+        })
+}
+
