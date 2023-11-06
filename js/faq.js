dddee0a580ac7917dd9c0ab9bcb826f6d2ae2f61
mikkype <miguelcaceda@gmail.com>
2023-11-06 03:25:40 +0100

    faq formulario js

---
+++ js/faq.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded",init)
+
+function init() {
+    console.log('DOM cargado')
+}
+
+function mostrarComentario() {
+    
+    var nombre = document.getElementById('form4Example1').value;
+    var comentario = document.getElementById('form4Example3').value;
+
+    if (nombre && comentario) {
+        var comentarioMostrado = document.getElementById('comentarioMostrado');
+        comentarioMostrado.innerHTML +=
+        ` 
+        <div class="row bg-body-tertiary alert alert-info">
+        <div class=col-12>
+        <p><b>${nombre}</b> dejó su comentario : ${comentario}</p>
+        </div>
+        </div>
+        `
+    }
+}
