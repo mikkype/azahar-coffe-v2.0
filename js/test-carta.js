248cf9443d7ad03bc8d423a42ef6fb2fa1bdeada
mikkype <miguelcaceda@gmail.com>
2023-11-06 20:49:38 +0100

    test

---
+++ js/test-carta.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", init)
+
+function init() {
+    console.log('DOM cargado')
+    //creamos una variable con el id
+    var mostrar = document.getElementById('container-carta');
+    var opciones = {
+        method: 'POST'
+    }
+    //ruta donde se consume el json
+    fetch('json/carta.json')
+        .then(result => result.json())
+        .then(data => {
+            console.log(test)
+            if (Array.isArray(test.carta)) {
+
+
+
+                data.carta.forEach(elemento => {
+
+                    mostrar.innerHTML += 
+                            `
+                            <p>${elemento.Nombre}</p>
+                            <p>${elemento.Descripción}</p>
+                            <p>${elemento.Precio}</p>
+                            `;
+
+                });
+            }
+
+
+        })
+}
