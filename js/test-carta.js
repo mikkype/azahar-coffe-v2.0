fe4a3de3e04abcc38d8c2e0decc33b7ef40aac98
mikkype <miguelcaceda@gmail.com>
2023-11-06 22:43:31 +0100

    carta dinamico ok

--- js/test-carta.js
+++ js/test-carta.js
@@ -1,33 +1,37 @@
 document.addEventListener("DOMContentLoaded", init)
+function init(){
+    console.log('DOM CARGADO')
 
-function init() {
-    console.log('DOM cargado')
-    //creamos una variable con el id
-    var mostrar = document.getElementById('container-carta');
-    var opciones = {
-        method: 'POST'
-    }
-    //ruta donde se consume el json
-    fetch('json/carta.json')
-        .then(result => result.json())
-        .then(test => {
-            console.log(test)
-            if (Array.isArray(test.carta)) {
-
-
-
-                test.carta.forEach(elemento => {
-
-                    mostrar.innerHTML += 
-                            `
-                            <p>${elemento.Nombre}</p>
-                            <p>${elemento.Descripción}</p>
-                            <p>${elemento.Precio}</p>
-                            `;
-
-                });
-            }
-
-
-        })
 }
+
+function cargarDatos() {
+    
+    fetch('json/carta.json')
+      .then(response => response.json())
+      .then(data => {
+        const elementosProductos = document.querySelectorAll('.producto');
+  
+        elementosProductos.forEach((elemento, index) => {
+          const producto = data.carta[index];
+  
+          if (producto) {
+            const nombreElemento = elemento.querySelector('.nombre');
+            const descripcionElemento = elemento.querySelector('.desc');
+            const precioElemento = elemento.querySelector('.prc');
+            
+  
+            nombreElemento.textContent = producto.Nombre;
+            descripcionElemento.textContent = `Descripción: ${producto.Descripción}`;
+            precioElemento.textContent = `Precio: ${producto.Precio}`;
+            
+          }
+        });
+      })
+      .catch(error => {
+        console.error('Hubo un error al cargar el archivo JSON:', error);
+      });
+  }
+cargarDatos();
+  
+
+
